11ac1161a3805290701414e053f96ff2e4442921
tushar-1910 <tusharagg19@gmail.com>
2022-08-09 15:59:17 +0530

    Loading ui added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,9 +4,10 @@ import { filterCountry, getcity, sortPopulation } from '../../Redux/City/action'
 import { getCountry } from '../../Redux/Country/action'
 import TransitionsModal from '../Edit/Edit'
 import Button from '@mui/material/Button';
+import { Skeleton } from '@mui/material'
 function Home() {
 
-    const { cities } = useSelector(store => store.cities)
+    const { cities, loading } = useSelector(store => store.cities)
     const { countries } = useSelector(store => store.countries)
     const dispatch = useDispatch()
     const [country, setCountry] = React.useState("");
@@ -31,18 +32,18 @@ function Home() {
     console.log(cities)
     return (
         <div style={{ margin: "Auto", width: "30rem" }}>
-            <select id="sort" onChange={(e)=>{dispatch(sortPopulation(e.target.value,country))}}>
+            <select id="sort" onChange={(e) => { dispatch(sortPopulation(e.target.value, country)) }}>
                 <option value="">Sort By</option>
                 <option value="asc">Ascending Order</option>
                 <option value="desc">Descending order</option>
             </select>
-            <select id="country" value={country} onChange={(e)=>{setCountry(e.target.value);dispatch(filterCountry(e.target.value))}}>
-          <option value="">Select Country</option>
-          {countries?.map(country => (
-            <option key={country.id} value={country.country}>{country.country}</option>
-          ))}
-        </select>
-        
+            <select id="country" value={country} onChange={(e) => { setCountry(e.target.value); dispatch(filterCountry(e.target.value)) }}>
+                <option value="">Select Country</option>
+                {countries?.map(country => (
+                    <option key={country.id} value={country.country}>{country.country}</option>
+                ))}
+            </select>
+
             <table className='table'>
                 <thead >
                     <tr>
@@ -55,14 +56,19 @@ function Home() {
                     </tr>
                 </thead>
                 <tbody>
-                    {cities.map((city,index) => (
+                    {loading ? <>
+                        <tr><td colSpan="6"><Skeleton animation="wave" /></td></tr>
+                        <tr><td colSpan="6"><Skeleton animation="wave" /></td></tr>
+                        <tr><td colSpan="6"><Skeleton animation="wave" /></td></tr>
+                        <tr><td colSpan="6"><Skeleton animation="wave" /></td></tr>
+                    </> : cities.map((city, index) => (
                         <tr key={city.id}>
-                            <td>{index+1}</td>
+                            <td>{index + 1}</td>
                             <td>{city.country}</td>
                             <td>{city.city}</td>
                             <td>{city.population}</td>
-                            <td><TransitionsModal item={city}/></td>
-                            <td><Button onClick={()=>{handleDelete(city.id)}}>Delete</Button></td>
+                            <td><TransitionsModal item={city} /></td>
+                            <td><Button onClick={() => { handleDelete(city.id) }}>Delete</Button></td>
                         </tr>
                     ))}
                 </tbody>
